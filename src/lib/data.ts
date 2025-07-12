65533c9cc3920edf1336543c23c93e2de544c293
Luis Africano <luis.africano@dendev.ar>
2025-07-12 19:15:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import type { Product, Category, ProductImageStructure } from './types';
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '@/lib/database.types';
